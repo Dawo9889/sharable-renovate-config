d6e5de5607ba8ef4c590e7325fc418aab3497bcf
Dawo9889 <dawid.gala@protonmail.com>
2026-02-17 08:32:50 +0100

    fix: Update Dockerfile and config to use correct image tags for Dependabot tests

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   hostRules: process.env.RENOVATE_GCP_DOCKER_PASSWORD
     ? [
         {
-          matchHost: 'europe-west3-docker.pkg.dev',
+          matchHost: 'europe-west3-docker.pkg.dev/sap-kyma-neighbors-dev/renovate-private-registry-test',
           hostType: 'docker',
           username: '_json_key_base64',
           password: process.env.RENOVATE_GCP_DOCKER_PASSWORD
